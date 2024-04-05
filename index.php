a0006c91898874612afe5efb6b27cbffb4b9c363
Jhon Martinez <jdmartinez364@misena.edu.co>
2024-04-05 10:14:30 -0500

    nombre

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ $dir = "posters/";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CRUD Modal</title>
+    <title>Proyecto Tabla</title>
 
     <link href="assets/css/bootstrap.min.css" rel="stylesheet">
     <link href="assets/css/all.min.css" rel="stylesheet">
